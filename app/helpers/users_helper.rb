a7f8fbc879c2a4872d99ca03459abdf39a27a6cf
jakoko <aerisgurlie777@gmail.com>
2015-03-06 16:59:27 -0800

    default pic for no profile picture

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,7 +1,8 @@
 module UsersHelper
 
   def user_profile_pic(user)
-    return user.profile_pic unless user.profile_pic.nil?
+    return user.profile_pic unless user.profile_pic.blank?
+    return image_url('no_image.jpeg')
   end
 
 end
